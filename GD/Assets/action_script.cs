53a94e67079b30d9c806b8c2f5c3a6894825432d
deredet <lucky.cahyadi@yahoo.com>
2017-03-30 13:26:03 +0700

    init

---
+++ GD/Assets/action_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class action_script : MonoBehaviour {
+	public GameObject player;
+	private Rigidbody2D r2D;
+	// Use this for initialization
+	void Start () {
+		r2D = player.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Jump(){
+		r2D.AddForce (new Vector2 (0f, 2f));
+	}
+}
---
+++ GD/Assets/player_move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+	private Rigidbody2D r2D;
+	// Use this for initialization
+	void Start () {
+		r2D = transform.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		r2D.AddForce(new Vector2(5f,0f));
+	}
+}
