a54fe8ae4bcfb945093652ddf051147b72702d0c
deredet <lucky.cahyadi@yahoo.com>
2017-05-09 23:04:10 +0700

    update main game

---
+++ GD/Assets/lifeTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifeTime : MonoBehaviour {
+	public float lifeTimer;
+	private float timer;
+	private GameObject temp;
+	private player_move player;
+	// Use this for initialization
+	void Start () {
+		timer = lifeTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	temp = GameObject.Find ("Player Sprite");
+		player = temp.GetComponent<player_move>();
+		if (player.alive) {
+			timer -= Time.deltaTime;
+			if (timer <=0)
+				Destroy (gameObject);
+		}
+
+	}
+}
